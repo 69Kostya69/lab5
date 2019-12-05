e6f547662ab9d9a3a0ee385b55a00f9d03966887
Kostya Salabay <extrimdysik@gmail.com>
2019-12-06 01:29:18 +0200

    first commit

---
+++ lab5.js
@@ -0,0 +1,17 @@
+const http = require('http')
+const hostname = '127.0.0.1'
+const fs = require('fs')
+const port = 3000
+
+fs.readFile('./index.html', (err, html) => {
+  if (err) {
+    throw err
+  }
+  http.createServer((req, res) => {
+  res.writeHeader(200, {"Content-Type" : "text/html"})
+  res.write(html)
+  res.end()
+  }).listen(port, hostname, () => {
+     console.log(`Server running at http://${hostname}:${port}/`)
+   })
+ })
